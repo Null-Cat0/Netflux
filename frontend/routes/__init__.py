38723c34c4512003f7e3dc5fb77cad84c4a619ec
Asier <asierserrano8@gmail.com>
2024-11-06 18:39:23 +0100

    Mi lista html

--- frontend/routes/__init__.py
+++ frontend/routes/__init__.py
@@ -1,9 +1,11 @@
 from .perfil_routes import perfil_bp
 from .usuario_routes import usuario_bp
 from .dispositivos_routes import dispositivos_bp
+from .contenidos_routes import contenidos_bp
 
 blueprints = [
     perfil_bp,
     usuario_bp,
     dispositivos_bp,
+    contenidos_bp
 ]
