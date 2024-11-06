78919561668e21f21ce7ebcfc59d2fdc452cf7b6
Asier <asierserrano8@gmail.com>
2024-11-06 18:41:18 +0100

    Create contenidos_routes.py

---
+++ frontend/routes/contenidos_routes.py
@@ -0,0 +1,11 @@
+import requests
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+
+from global_config import ContenidosConfig as contConf
+
+contenidos_bp = Blueprint('contenidos', __name__)
+
+@contenidos_bp.route('/mi_lista')
+def mi_lista():
+    return render_template("mi_lista.html")
+
