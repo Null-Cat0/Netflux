9103ae79f7b0777d9e5a9738e68c8a50f5af60b2
Deluwuta <albertotrejoats149@gmail.com>
2024-11-22 16:23:16 +0100

    Ajustes en cómo se obtiene el historial de un perfil

--- usuariosflask/openapi_server/models/historial_perfil_db.py
+++ usuariosflask/openapi_server/models/historial_perfil_db.py
@@ -1,4 +1,5 @@
 from openapi_server import db
+from datetime import datetime
 
 class HistorialPerfilDB(db.Model):
 
@@ -7,6 +8,7 @@ class HistorialPerfilDB(db.Model):
     perfil_id = db.Column(db.Integer, db.ForeignKey('perfil.perfil_id'), nullable=False)
     contenido = db.Column(db.String(255), nullable=False)
     es_capitulo = db.Column(db.Boolean, nullable=False, default=False)
+    fecha_visualizacion = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     __table_args__ = (
         db.PrimaryKeyConstraint('perfil_id', 'contenido'),
@@ -16,4 +18,4 @@ class HistorialPerfilDB(db.Model):
         self.perfil_id = perfil_id
         self.contenido = contenido
         self.es_capitulo = es_capitulo
-    
+        self.fecha_visualizacion = datetime.now()
